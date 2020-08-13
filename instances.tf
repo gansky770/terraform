be5fbd51043c1ad813c521b0b017c48b9ecd9e3d
Moti Gansky <Moti.Gansky@Strauss-Group.com>
2020-08-13 19:55:19 +0300

    e

--- instances.tf
+++ instances.tf
@@ -5,16 +5,22 @@ instance_type = "t2.micro"
 
 resource "aws_instance" "moti2" {
 ami = var.AMIS[var.AWS_REGION]
-instance_type = "t3.micro"
+instance_type = "t2.micro"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "moti-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_security_group_rule" "moti-sec-rule" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = 0.0.0.0/0
+  security_group_id = "sg-fc5d87bc"
 }
 
+# key pair TODO
+
+
+# remote exec 
+      # "sudo apt-get -y update",
+      # "sudo apt-get -y install nginx",
+      # "sudo service nginx start",
--- vars.tf
+++ vars.tf
@@ -9,7 +9,7 @@ type = map(string)
 default = {
 us-east-1 = "ami-13be557e"
 us-west-2 = "ami-06b94666"
-eu-west-1 = "ami-047bb4163c506cd98"
+eu-west-1 = "ami-08f51d843bf37ded6"
 }
 }
 
